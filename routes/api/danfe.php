73b4ee2f530b3c6230017fc778b70af6fac03bba
Iago Melo <iagosilvamelo@gmail.com>
2020-03-03 11:07:14 -0300

    Criado métodos para envio do objeto a api.

--- routes/api/danfe.php
+++ routes/api/danfe.php
@@ -3,5 +3,6 @@
 $router->group(['prefix' => 'danfe'], function($router) {
 
     $router->get('verify-mail', 'DanfeController@index');
+    $router->get('send',        'MainController@send_danfes_to_api');
 
 });
